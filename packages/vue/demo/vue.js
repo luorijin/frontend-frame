3cbef82ce1a651c13e9ac95691d7d493d25c43ee
luo <rjluo@ewaytec.cn>
2019-08-08 09:56:36 +0800

    例子

--- packages/vue/demo/vue.js
+++ packages/vue/demo/vue.js
@@ -1,6 +1,6 @@
 /*!
  * vue.js v1.0.0
- * (c) 2018-2018 luo
+ * (c) 2018-2019 luo
  * Released under the MIT License.
  */
 (function(l, i, v, e) { v = l.createElement(i); v.async = 1; v.src = '//' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; e = l.getElementsByTagName(i)[0]; e.parentNode.insertBefore(v, e)})(document, 'script');
